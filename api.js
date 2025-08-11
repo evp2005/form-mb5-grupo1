f8454711dae84b93c3ba0eea48770f4f140b2832
Damaris-Gutierrez <damarisgutierrez202@gmail.com>
2025-08-11 16:43:26 -0500

    Agregué los 2 archivos para el API simulada

---
+++ api.js
@@ -0,0 +1,17 @@
+const URL = 'http://localhost:3001/mensajes';
+
+export async function enviarDatos(datos) {
+  const respuesta = await fetch(URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(datos)
+  });
+
+  if (!respuesta.ok) {
+    throw new Error('Error al enviar los datos');
+  }
+
+  return await respuesta.json();
+}
